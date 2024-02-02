d30e7f784a99d4d03d8376b608b0001776a38b4f
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-02 18:31:05 +0000

    Project 1 Version 1

---
+++ PROJECTS/project1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  cout << "RECIPE PROGRAM" << endl;
+
+  cout << "--------------------------------------------------------------------------------" << endl;
+  cout << "Receipt 1: 3-ingredient Mac & Cheese" << endl;
+  cout << "From https://tasty.co/recipe/3-ingredient-mac-cheese" << endl << endl;
+  cout << "INGREDIENTS:" << endl;
+  cout << "* 5 cups milk" << endl;
+  cout << "* 1 lb elbow macoroni" << endl;
+  cout << "* 2 cubes shredded cheddar cheese" << endl << endl;
+  cout << "--------------------------------------------------------------------------------" << endl;
+  
+  cout << "Receipt 2: 3-Ingredient Banana Bread" << endl;
+  cout << "From https://www.tasteofhome.com/article/3-ingredient-banana-bread/" << endl << endl;
+  cout << "INGREDIENTS:" << endl;
+  cout << "* 3-4 very ripe bananas" << endl;
+  cout << "* 1 box (15.25 ounces) yellow cake mix" << endl;
+  cout << "* 2 large eggs" << endl << endl;
+
+
+  cout << "THE END" << endl;
+  return 0;
+}
