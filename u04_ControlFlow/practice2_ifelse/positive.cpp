26d0edb543b89eec7dfdc6669bff5afec1ec6d83
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-10 13:58:13 +0000

    Update positive.cpp

--- u04_ControlFlow/practice2_ifelse/positive.cpp
+++ u04_ControlFlow/practice2_ifelse/positive.cpp
@@ -9,10 +9,15 @@ using namespace std;  // Using the C++ STanDard libraries
 int main()
 {
   // TODO: Create an integer `number` variable. Ask the user to enter a value for the number.
-
+  int number;
+  cout << "Please enter the value for this number: " << endl;
+  cin >> number;
 
   // TODO: If number is greater than 0, then display "Positive number". Otherwise, display "Negative or zero".
-
+if (number > 0){
+	cout << "Positive Number!" << endl;}
+	else {
+		cout << "Not Postitive!" << endl;}
 
 
   // Return 0 means quit program with no errors, in this context.
