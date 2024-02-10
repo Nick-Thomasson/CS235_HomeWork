a6d0dd281a9d5f586adfb1246663f861ae363a94
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-10 13:57:43 +0000

    Update balance.cpp

--- u04_ControlFlow/practice1_if/balance.cpp
+++ u04_ControlFlow/practice1_if/balance.cpp
@@ -12,19 +12,28 @@ int main()
   cout << fixed << setprecision( 2 ); // Format floats like USD
 
   // TODO: Create three float variables: `balance`, `withdraw`, and `remaining`.
-  
+  float balance;
+  float withdraw;
+  float remaining;
 
   // TODO: Use `cout` to prompt the user to enter information and `cin` to get information from the keyboard.
   // Get user's input for `balance` (their current bank balance) and `withdraw` (how much to withdraw from bank).
-
+  cout << "Please enter your current bank balance: " << endl;
+  cin >> balance;
+  cout << "Please enter the amount you'd like to withdraw:" << endl;
+  cin >> withdraw;
+ 
 
   // TODO: Calculate the `remaining` balance after the withdraw with `remaining = balance - withdraw;`
+  remaining = balance - withdraw;
 
 
   // TODO: Display "Remaining balance: $" and the `remaining` variable.
-
+	cout << "Remaining balance : $" << remaining << endl;
 
   // TODO: If `remaining` is less than 0, then also display "(OVERDRAWN)".
+	if (remaining < 0){
+		cout << "OVERDRAWN!" << endl;}
 
 
   // Return 0 means quit program with no errors, in this context.
