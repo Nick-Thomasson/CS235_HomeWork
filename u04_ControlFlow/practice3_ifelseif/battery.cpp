1878088fdb26219f4ebf4846110a5e136417b5e2
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-10 13:58:36 +0000

    Update battery.cpp

--- u04_ControlFlow/practice3_ifelseif/battery.cpp
+++ u04_ControlFlow/practice3_ifelseif/battery.cpp
@@ -8,19 +8,28 @@ using namespace std;  // Using the C++ STanDard libraries
 // - PROGRAM CODE -----------------------------------------------------------//
 int main()
 {
-  // TODO: Create an int variable named `charge`.
+// TODO: Create an int variable named `charge`.
+int charge;
 
+// TODO: Use `cout` to tell the user to enter their battery charge %.
+cout << "Please enter your battery percentage: " << endl;
 
-  // TODO: Use `cout` to tell the user to enter their battery charge %.
+// TODO: Use `cin` to get their input and store it in the `charge` variable.
+cin >> charge;
 
+// TODO: Use if/else if/else statements to decide which battery image to draw.
+// 90% and above: [****];  75% and above: [***-];  50% and above: [**--];  25% and above: [*---];  below 25%: [----]
+if (charge >= 90){
+cout << "90% and above: [****]" << endl;}
+else if (charge >= 75){
+cout << "75% and above: [***-]" << endl;}
+else if (charge >= 50){
+cout << "50% and above: [**--]" << endl;}
+else if (charge >= 25){
+cout << "25% and above: [*---]" << endl;}
+else {cout << "below 25%: [----]" << endl;}
+	
 
-  // TODO: Use `cin` to get their input and store it in the `charge` variable.
-
-
-  // TODO: Use if/else if/else statements to decide which battery image to draw.
-  // 90% and above: [****];  75% and above: [***-];  50% and above: [**--];  25% and above: [*---];  below 25%: [----]
-
-
-  // Return 0 means quit program with no errors, in this context.
-  return 0;
+// Return 0 means quit program with no errors, in this context.
+return 0;
 }
