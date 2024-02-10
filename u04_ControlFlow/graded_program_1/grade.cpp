6616d7c1f0117c806fe1aa5621451a03ad4299a6
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-10 16:16:46 +0000

    Update grade.cpp

--- u04_ControlFlow/graded_program_1/grade.cpp
+++ u04_ControlFlow/graded_program_1/grade.cpp
@@ -5,24 +5,43 @@
 using namespace std;  // Using the C++ STanDard libraries
 
 // - STUDENT CODE -----------------------------------------------------------//
-const string YOURNAME = "Your Name, Spring 2024"; // TODO: Update this to your name!
+const string YOURNAME = "Nick Thomasson, Spring 2024"; // TODO: Update this to your name!
 
 char StudentCode( float points_possible, float my_score )
 {
-  char letter_grade = 'F';
-  
-  // TODO: Create a float for `grade_percent`. Calculate the percent with `my_score` divided by `points_possible` times 100.
+char letter_grade = 'F';
 
+// TODO: Create a float for `grade_percent`. Calculate the percent with `my_score` divided by `points_possible` times 100.
+float grade_percent;
+grade_percent = (my_score / points_possible) * 100;
 
-  // TODO: Use if/else if/else statements to determine value for `letter_grade` based on the `grade_percent`;
-  // 89.5 and above = A, 79.5 and above = B, 69.5 and above = C, 59.5 and above = D, below that is F.
+// TODO: Use if/else if/else statements to determine value for `letter_grade` based on the `grade_percent`;
+// 89.5 and above = A, 79.5 and above = B, 69.5 and above = C, 59.5 and above = D, below that is F.
 
+if (grade_percent > 89.5){
+cout << "Your grade letter is A" << endl;
+letter_grade = 'A';}
 
-  // TODO: Display grade information, including `my_score`, `points_possible`, `grade_percent`, and `letter_grade`.
+else if (grade_percent > 79.5){
+cout << "Your grade letter is B" << endl;
+letter_grade = 'B';}
+else if (grade_percent > 69.5){
+cout << "Your grade letter is C" << endl;
+letter_grade = 'C';}
+else if (grade_percent > 59.5){
+cout << "Your grade letter is D" << endl;
+letter_grade = 'D';}
+else {
+cout << "Your grade letter is F" << endl;}
 
+// TODO: Display grade information, including `my_score`, `points_possible`, `grade_percent`, and `letter_grade`.
+cout << "My Score: " << my_score << endl;
+cout << "Points Possible: " << points_possible << endl;
+cout << "Grade percent: " << grade_percent << endl;
+cout << "Letter Grade: " << letter_grade << endl;
 
-  // TODO: Return the `letter_grade` as the result.
-  return letter_grade;
+// TODO: Return the `letter_grade` as the result.
+return letter_grade;
 }
 
 
@@ -34,114 +53,114 @@ char StudentCode( float points_possible, float my_score )
 void Tester();
 int main( int argCount, char* args[] )
 {
-  cout << fixed << setprecision( 2 );
-  std::cout << "1. Run AUTOMATED TESTS" << std::endl;
-  std::cout << "2. Run PROGRAM" << std::endl;
-  int choice;
-  std::cout << ">> ";
-  std::cin >> choice;
+cout << fixed << setprecision( 2 );
+std::cout << "1. Run AUTOMATED TESTS" << std::endl;
+std::cout << "2. Run PROGRAM" << std::endl;
+int choice;
+std::cout << ">> ";
+std::cin >> choice;
 
-  switch( choice )
-  {
-    case 1:
-    Tester();
-    break;
+switch( choice )
+{
+case 1:
+Tester();
+break;
 
-    case 2:{
-      float points_possible, my_score;
-      cout << "Enter points possible: ";
-      cin >> points_possible;
-      cout << "Enter your score: ";
-      cin >> my_score;
-      
-      char letter_grade = StudentCode( points_possible, my_score );
-      cout << "RESULT: " << letter_grade << endl;
-    }
-    break;
-  }
-  
-  /*
-  if ( argCount == 2 && string( args[1] ) == "test" )
-  {
-    // Run tests
-    Tester();
-  }
-  else if ( argCount == 3 )
-  {
-    // Run program
-    char letter_grade = StudentCode( stof( args[1] ), stof( args[2] ) );
-    cout << "RESULT: " << letter_grade << endl;
-  }
-  else
-  {
-    cout << "argCount is " << argCount << "; NOT ENOUGH ARGUMENTS!" << endl;
-    cout << "Run tests:      " << args[0] << " test" << endl;
-    cout << "Run as program: " << args[0] << " points_possible my_score" << endl;
-    return 1;
-  }
-  * */
+case 2:{
+float points_possible, my_score;
+cout << "Enter points possible: ";
+cin >> points_possible;
+cout << "Enter your score: ";
+cin >> my_score;
 
-  return 0;
+char letter_grade = StudentCode( points_possible, my_score );
+cout << "RESULT: " << letter_grade << endl;
+}
+break;
+}
+
+/*
+if ( argCount == 2 && string( args[1] ) == "test" )
+{
+// Run tests
+Tester();
+}
+else if ( argCount == 3 )
+{
+// Run program
+char letter_grade = StudentCode( stof( args[1] ), stof( args[2] ) );
+cout << "RESULT: " << letter_grade << endl;
+}
+else
+{
+cout << "argCount is " << argCount << "; NOT ENOUGH ARGUMENTS!" << endl;
+cout << "Run tests:      " << args[0] << " test" << endl;
+cout << "Run as program: " << args[0] << " points_possible my_score" << endl;
+return 1;
+}
+* */
+
+return 0;
 }
 
 // - AUTOMATED TESTER -------------------------------------------------------//
 void Tester()
 {
-  const string GRN = "\033[0;32m"; const string RED = "\033[0;31m"; const string BOLD = "\033[0;35m"; const string CLR = "\033[0m";
-  cout << "2024-01-U04-P1-TEST; STUDENT: " << YOURNAME << endl;
+const string GRN = "\033[0;32m"; const string RED = "\033[0;31m"; const string BOLD = "\033[0;35m"; const string CLR = "\033[0m";
+cout << "2024-01-U04-P1-TEST; STUDENT: " << YOURNAME << endl;
 
-  // (Automated test):
+// (Automated test):
 
-  const int TOTAL_TESTS = 5;
-  float in1[TOTAL_TESTS]; // inputs 1
-  float in2[TOTAL_TESTS]; // inputs 2
-  char  exo[TOTAL_TESTS]; // expected output
-  char  aco[TOTAL_TESTS]; // actual output
+const int TOTAL_TESTS = 5;
+float in1[TOTAL_TESTS]; // inputs 1
+float in2[TOTAL_TESTS]; // inputs 2
+char  exo[TOTAL_TESTS]; // expected output
+char  aco[TOTAL_TESTS]; // actual output
 
-  // Setup test 1
-  in1[0] = 89;
-  in2[0] = 90;
-  exo[0] = 'A';
+// Setup test 1
+in1[0] = 89;
+in2[0] = 90;
+exo[0] = 'A';
 
-  // Setup test 2
-  in1[1] = 80;
-  in2[1] = 64;
-  exo[1] = 'B';
+// Setup test 2
+in1[1] = 80;
+in2[1] = 64;
+exo[1] = 'B';
 
-  // Setup test 3
-  in1[2] = 70;
-  in2[2] = 49;
-  exo[2] = 'C';
+// Setup test 3
+in1[2] = 70;
+in2[2] = 49;
+exo[2] = 'C';
 
-  // Setup test 4
-  in1[3] = 60;
-  in2[3] = 36;
-  exo[3] = 'D';
+// Setup test 4
+in1[3] = 60;
+in2[3] = 36;
+exo[3] = 'D';
 
-  // Setup test 5
-  in1[4] = 59;
-  in2[4] = 12;
-  exo[4] = 'F';
+// Setup test 5
+in1[4] = 59;
+in2[4] = 12;
+exo[4] = 'F';
 
-  // Run tests
-  for ( int i = 0; i < TOTAL_TESTS; i++ )
-  {
-    aco[i] = StudentCode( in1[i], in2[i] );
+// Run tests
+for ( int i = 0; i < TOTAL_TESTS; i++ )
+{
+aco[i] = StudentCode( in1[i], in2[i] );
 
-    if ( aco[i] == exo[i] )
-    {
-      // PASS
-      cout << GRN << "[PASS] ";
-      cout << " TEST " << i+1 << ", StudentCode(" << in1[i] << ", " << in2[i] << ") = " << aco[i] << endl;
-    }
-    else
-    {
-      // FAIL
-      cout << RED << "[FAIL] ";
-      cout << " TEST " << i+1 << ", StudentCode(" << in1[i] << ", " << in2[i] << ")" << endl;
-      cout << "   EXPECTED OUTPUT: [" << exo[i] << "]" << endl;
-      cout << "   ACTUAL OUTPUT:   [" << aco[i] << "]" << endl;
-    }
-  }
-  cout << CLR;
+if ( aco[i] == exo[i] )
+{
+// PASS
+cout << GRN << "[PASS] ";
+cout << " TEST " << i+1 << ", StudentCode(" << in1[i] << ", " << in2[i] << ") = " << aco[i] << endl;
+}
+else
+{
+// FAIL
+cout << RED << "[FAIL] ";
+cout << " TEST " << i+1 << ", StudentCode(" << in1[i] << ", " << in2[i] << ")" << endl;
+cout << "   EXPECTED OUTPUT: [" << exo[i] << "]" << endl;
+cout << "   ACTUAL OUTPUT:   [" << aco[i] << "]" << endl;
+}
+}
+cout << CLR;
 }
