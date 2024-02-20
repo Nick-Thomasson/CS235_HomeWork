acfaf99db6f21c1baf65cbe7d6aec6a64a3458d4
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-20 16:06:43 +0000

    Update Ingredient.h

--- u05_Structs/practice2_struct/Ingredient.h
+++ u05_Structs/practice2_struct/Ingredient.h
@@ -1,10 +1,19 @@
 #ifndef _INGREDIENT
 #define _INGREDIENT
-
+#include <iostream>   
+#include <iomanip>
+#include <string>  
+using namespace std;
 // - STRUCT DECLARATION -----------------------------------------------------//
 // TODO: Declare an Ingredient struct,
 // It should have a string for `name`,
 // a string for `unit`
 // and a float for `amount`.
+
+struct Ingredient{
+    string name;
+    string unit;
+    float amount;
+};
 
 #endif
