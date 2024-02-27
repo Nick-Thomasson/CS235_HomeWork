0781e9c6de96e6fa87c7c9494e6514a749e171c3
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-27 02:20:41 +0000

    Update program2.cpp

--- u06_Functions/practice2_YesInNoOut/program2.cpp
+++ u06_Functions/practice2_YesInNoOut/program2.cpp
@@ -1,14 +1,22 @@
 // PROGRAM: Practice with function - YES input, NO output
 #include <iostream>
+
 #include <iomanip>
+
 using namespace std;
+float myPrice;
 
 // -- FUNCTION DEFINITION GOES HERE -------------------------------------------
+void FormatUSD(float price) {
+  cout << fixed << setprecision(2);
+  cout << "You entered the amount of $" << price << endl;
+}
 
-
-int main()
-{
+int main() {
   // -- IMPLEMENT PROGRAM HERE --------------------------------------------------
-
+  float myPrice;
+  cout << "Please enter an amount of money: " << endl;
+  cin >> myPrice;
+  FormatUSD(myPrice);
   return 0;
 }
