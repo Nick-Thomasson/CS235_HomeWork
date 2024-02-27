4f6e686cd57844eda2f53eebd430de76c87455d9
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-27 02:20:03 +0000

    Update program1.cpp

--- u06_Functions/practice1_NoInNoOut/program1.cpp
+++ u06_Functions/practice1_NoInNoOut/program1.cpp
@@ -1,15 +1,21 @@
 // PROGRAM: Practice with function - NO input, NO output
 #include <iostream>
+
 #include <iomanip>
+
 using namespace std;
 
 // -- FUNCTION DEFINITION GOES HERE -------------------------------------------
 
+void DisplayMenu() {
+  cout << "1. Add item: " << endl;
+  cout << "2. Checkout: " << endl;
 
-int main()
-{
+}
+
+int main() {
   // -- IMPLEMENT PROGRAM HERE --------------------------------------------------
-
+  DisplayMenu();
 
   return 0;
 }
