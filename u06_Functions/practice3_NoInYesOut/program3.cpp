23275c4d909af79e042b65a58a0525946465c075
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-27 02:21:25 +0000

    Update program3.cpp

--- u06_Functions/practice3_NoInYesOut/program3.cpp
+++ u06_Functions/practice3_NoInYesOut/program3.cpp
@@ -1,14 +1,18 @@
 // PROGRAM: Practice with function - NO input, YES output
 #include <iostream>
+
 #include <string>
+
 using namespace std;
 
 // -- FUNCTION DEFINITION GOES HERE -------------------------------------------
+float getTaxPercent() {
+  return 9.61;
+}
 
-
-int main()
-{
+int main() {
   // -- IMPLEMENT PROGRAM HERE --------------------------------------------------
-
+  float myTax = getTaxPercent();
+  cout << "Tax rate is: " << myTax << endl;
   return 0;
 }
