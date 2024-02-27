3e377670938bd1fc933a429655ef8f6c64cb27fc
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-02-27 02:22:10 +0000

    Update program4.cpp

--- u06_Functions/practice4_YesInYesOut/program4.cpp
+++ u06_Functions/practice4_YesInYesOut/program4.cpp
@@ -1,15 +1,26 @@
 // PROGRAM: Practice with function - YES input, YES output
 #include <iostream>
+
 #include <string>
+
 using namespace std;
 
 // -- FUNCTION DEFINITION GOES HERE -------------------------------------------
+float GetPricePlusTax(float original_price, float tax_percent) {
+  return original_price + (tax_percent / 100 * original_price);
+}
 
+int main() {
+  float price, tax, updated_price;
+  cout << "Enter Price: $";
+  cin >> price;
+  cout << "Enter tax: %";
+  cin >> tax;
 
-int main()
-{
-  // -- IMPLEMENT PROGRAM HERE --------------------------------------------------
+  updated_price = GetPricePlusTax(price, tax);
+  cout << "You entered " << price << " for your price, " << "and " << tax << " for your tax." << endl;
 
+  cout << "Your updated price is: " << updated_price << endl;
 
   return 0;
 }
