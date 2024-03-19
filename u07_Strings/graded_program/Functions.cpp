a31c1ea4b43e5f74bd9c197ac671558b7acf3c03
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-03-19 16:37:44 +0000

    Update Functions.cpp

--- u07_Strings/graded_program/Functions.cpp
+++ u07_Strings/graded_program/Functions.cpp
@@ -6,63 +6,55 @@ using namespace std;  // Using the C++ STanDard libraries
 
 unsigned int GetStringLength( string text )
 {
-  // TODO: Use the string's size function to return the size of the `text` string.
-  // Docs: https://cplusplus.com/reference/string/string/size/
-  return 0; // PLACEHOLDER - REMOVE ME!
+	return text.size();
+
 }
 
 char GetLetter( string text, unsigned int position )
 {
-  // TODO: Use the string's subscript operator to return a letter in `text` at the `position` specified.
-  // Docs: https://cplusplus.com/reference/string/string/size/
-  return 'x'; // PLACEHOLDER - REMOVE ME!
+	return text[position];
 }
 
 int GetSubstringPosition( string haystack, string needle )
 {
-  // TODO: Use the string's find function to see if `needle` is in the `haystack`; return the position.
-  // Docs: https://cplusplus.com/reference/string/string/find/
-  return -1; // PLACEHOLDER - REMOVE ME!
+
+	return haystack.find(needle);
+
 }
 
 string CombineStrings( string str1, string str2 )
 {
-  // TODO: Use the string's concatenation operator to combine `str1` and `str2` and return the result.
-  // Docs: https://cplusplus.com/reference/string/string/operator+/
-  return "x"; // PLACEHOLDER - REMOVE ME!
+	return str1 + str2;
 }
 
 int CompareStrings( string str1, string str2 )
 {
-  // TODO: Use the string's compare function to compare `str1` and `str2` and return the result.
-  // Docs: https://cplusplus.com/reference/string/string/compare/
-  return 0; // PLACEHOLDER - REMOVE ME!
+
+  return str1.compare(str2);
 }
 
 string StringRelations( string str1, string str2 )
 {
-  // TODO: Use the relational operators < and > to return "[str1] < [str2]" or "[str1] > [str2]" or "[str1] == [str2]".
-  // Docs: https://cplusplus.com/reference/string/string/operators/
-  return "x"; // PLACEHOLDER - REMOVE ME!
+  if (str1 < str2) {
+        return " < ";
+    } else if (str1 > str2) {
+        return " > ";
+    } else {
+        return " == ";
+    }
 }
 
 string InsertString( string original, string insert_me, int position )
 {
-  // TODO: Use the string's insert function to insert the `insert_me` string into the `original` string at the `position` given. Return the result.
-  // Docs: https://cplusplus.com/reference/string/string/insert/
-  return "x"; // PLACEHOLDER - REMOVE ME!
+  return original.insert(position, insert_me);
 }
 
 string EraseFromString( string original, int position, int length )
 {
-  // TODO: Use the string's erase function to remove text from the `original` string, starting at the `position` given, and going for `length` amount of characters. Return the result.
-  // Docs: https://cplusplus.com/reference/string/string/insert/
-  return "x"; // PLACEHOLDER - REMOVE ME!
+  return original.erase(position, length);
 }
 
 string ReplaceInString( string original, string insert_me, int position, int length )
 {
-  // TODO: Use the string's replace function to replace text in the `original` string` at `position`, going for the `length` given, replacing the text with the `insert_me` string. Return the result.
-  // Docs: https://cplusplus.com/reference/string/string/replace/
-  return "x"; // PLACEHOLDER - REMOVE ME!
+  return original.replace(position, length, insert_me);
 }
