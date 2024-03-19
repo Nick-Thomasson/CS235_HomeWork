331764a1971843985b379cb8ddd585448390b9ac
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-03-19 20:18:44 +0000

    Update saveload.cpp

--- u08_FileIO/practice5_saveload/saveload.cpp
+++ u08_FileIO/practice5_saveload/saveload.cpp
@@ -14,19 +14,30 @@ int main()
   cout << "Loading game..." << endl;
 
   // TODO: Create ifstream object named `input`. Try to open "savegame.txt".
+  ifstream input;
+  input.open("savegame.txt");
 
 
   // TODO: If `input.fail()` is true, then display message that savegame not found - starting with default.
   // Set `name` to "Default Knight" (or whatever you want)
   // Set `level` to 1
+  if (input.fail()) {
+	  cout << "Couldn't find savegame, creating a new character...";
+	  name = "Default Knight";
+	  level = 1;
+  }
 
   // TODO: Else, if `input.fail()` was false (no fail),
   // Use `getline` to load from `input` to `name`, then
   // use `input >>` to load from `input` to `level`
+  else {
+	  getline(input, name);
+	  input >> level;
+  }
 
 
   // TODO: Close `input` once done loading game
-
+  input.close();
 
   cout << "YOUR CHARACTER" << endl;
   cout << "Name:  " << name << endl;
@@ -39,10 +50,13 @@ int main()
   cout << endl << "Saving game..." << endl;
 
   // TODO: Create ofstream object named `output`. Open "savegame.txt".
+  ofstream output;
+  output.open("savegame.txt");
 
 
   // TODO: Output `name` and `level` to the `output` file, each on their own lines.
-
+  output << name << endl;
+  output << level << endl;
 
   return 0;
 }
