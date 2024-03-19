ff256dc1c54ee109b5d0257e6104bfeefe21e9dc
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-03-19 19:29:51 +0000

    Update load2.cpp

--- u08_FileIO/practice3_loadline/load2.cpp
+++ u08_FileIO/practice3_loadline/load2.cpp
@@ -11,19 +11,24 @@ int main()
   string read;
 
   // TODO: Create a variable, type is ifstream, name is `input`.
+  ifstream input;
 
 
   // TODO: Use `input`'s open function to open "lyricsA.txt".
+  input.open("lyricsA.txt");
 
 
   // TODO: Use `getline( input, read );` to read one line of text from the file.
-
+	getline(input, read);
 
   // TODO: Use `cout` to display the contents of the `read` variable.
-
+	cout << read << endl;
 
   // TODO: Do this twice more.
-
+	getline(input, read);
+	cout << read << endl;
+	getline(input, read);
+	cout << read << endl;
 
 
 
