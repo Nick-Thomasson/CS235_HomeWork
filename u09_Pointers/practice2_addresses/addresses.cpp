63ca96764769b43898396255536d7f997d3a421e
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-03-24 22:00:05 +0000

    Update addresses.cpp

--- u09_Pointers/practice2_addresses/addresses.cpp
+++ u09_Pointers/practice2_addresses/addresses.cpp
@@ -10,11 +10,28 @@ int main()
   int int1, int2, int3, int4, int5;
 
   cout << "int1's address is: " << &int1 << endl;
-  // TODO: Display the address of each int variable
+  cout << endl;
+  cout << "int2's address is: " << &int2 << endl;
+  cout << endl;
+  cout << "int3's address is: " << &int3 << endl;
+  cout << endl;
+  cout << "int4's address is: " << &int4 << endl;
+  cout << endl;
+  cout << "int5's address is: " << &int5 << endl;
   cout << endl;
 
+
+
+
   cout << "bool1's address is: " << &bool1 << endl;
-  // TODO: Display the address of each bool variable
+  cout << endl;
+  cout << "bool2's address is: " << &bool2 << endl;
+  cout << endl;
+  cout << "bool3's address is: " << &bool3 << endl;
+  cout << endl;
+  cout << "bool4's address is: " << &bool4 << endl;
+  cout << endl;
+  cout << "bool5's address is: " << &bool5 << endl;
   cout << endl;
 
 
