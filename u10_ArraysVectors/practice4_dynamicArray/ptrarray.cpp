d82a9231c0c5cdaf6a061b3213893dc6d28148c3
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-03-28 00:31:06 +0000

    Update ptrarray.cpp

--- u10_ArraysVectors/practice4_dynamicArray/ptrarray.cpp
+++ u10_ArraysVectors/practice4_dynamicArray/ptrarray.cpp
@@ -1,38 +1,37 @@
-// PROGRAM: Practice creating a dynamic array using a pointer
 #include <iostream>
-#include <array>
 #include <string>
 using namespace std;
 
 int main()
 {
-  // TODO: Declare a size_t variable named total_classes.
+    // Declare a size_t variable named total_classes
+    size_t total_classes;  
+
+    // Ask the user how many classes they have. Store their answer in total_classes.
+    cout << "How many classes do you have? ";
+    cin >> total_classes;
+
+    // Use a pointer to allocate space for a new dynamic array of strings.
+    string* my_classes = new string[total_classes];
+
+    cout << "Getting input:" << endl;
 
 
-  // TODO: Ask the user how many classes they have. Store their answer in total_classes.
-  cout << "How many classes do you have? ";
-  cin >> total_classes;
+    // Get user input and store it in the array at my_classes[i]
+    // Use a for loop from i = 0 to total_classes
+    for (size_t i = 0; i < total_classes; i++) {
+        cout << "Enter name for class #" << i << ": ";
+        cin >> my_classes[i];  
+    }
 
-  // TODO: Use a pointer to allocate space for a new dynamic array of integers.
-  // The size should be total_classes.
+    cout << endl << "Resulting array:" << endl;
 
+    // Use a for loop from i = 0 to total_classes
+    for (size_t i = 0; i < total_classes; i++) {
+        cout << "Class #" << i << ": " << my_classes[i] << endl;  // Display the index and element
+    }
 
-  cout << "Getting input:" << endl;
+    delete[] my_classes;  
 
-
-  // TODO: Use a for loop from i = 0 to my_classes.size().
-  // Within the loop, ask the user to enter class # i, get their input and store it in the array at my_classes[i].
-
-
-  cout << endl << "Resulting array:" << endl;
-
-
-  // TODO: Use a for loop from i = 0 to my_classes.size().
-  // Within the loop, display the index (i) and element (my_classes[i]).
-
-
-  // TODO: Free the memory at the my_classes address.
-
-
-  return 0;
+    return 0;
 }
