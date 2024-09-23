2c635a5920c5b6d7ebfd9f117d23781cf8c6783d
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-09-23 14:23:41 -0500

    final

--- u07_StandardTemplateLibrary/practice4_map/practice_4 map/map.cpp
+++ u07_StandardTemplateLibrary/practice4_map/practice_4 map/map.cpp
@@ -12,18 +12,23 @@ int main()
 
   // - STUDENT CODE ----------------------------------------------------------
   cout << endl << "Declare a map with string keys and float values..." << endl;
-  
+  map<string, float>myMap;
   // -------------------------------------------------------------------------
 
 
   // - STUDENT CODE ----------------------------------------------------------
   cout << endl << "Set up 3 product names and their prices in the map..." << endl;
-  
+  myMap["Hotdogs"] = 3.99;
+  myMap["Milk"] = 3.47;
+  myMap["Cereal"] = 2.99;
+
   // -------------------------------------------------------------------------
 
   // - STUDENT CODE ----------------------------------------------------------
   cout << endl << "Iterating over the map to display each element's key and value..." << endl;
-  
+  for (const auto& temp : myMap) {
+	  cout << temp.first  << " - $" << temp.second << endl;
+  }
   // -------------------------------------------------------------------------
 
 
@@ -33,7 +38,8 @@ int main()
 
   // - STUDENT CODE ----------------------------------------------------------
   cout << "The price of this item is... $";
-  
+  cout << myMap[choice];
+
   // -------------------------------------------------------------------------
 
 
