b0b1e1694ba20af928c9ac539099c90cad4008c4
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-10-07 15:05:07 -0500

    update

--- u11_Templates_Exceptions/graded_program/Project1/Utilities.h
+++ u11_Templates_Exceptions/graded_program/Project1/Utilities.h
@@ -11,19 +11,29 @@ using namespace std;
 
 // - STUDENT CODE ----------------------------------------------------------
 //! TODO: Replace these with a templated function
+
+template <typename T>
+void DisplayAll(vector<T> objects) {
+	for (size_t i = 0; i < objects.size(); i++) {
+		cout << i << ". ";
+		objects[i].Display(); 
+	}
+}
+
+/*
 void DisplayAll( vector<Order> objects );
 
 void DisplayAll( vector<PaymentMethod> objects );
 
-void DisplayAll( vector<Product> objects );
+void DisplayAll( vector<Product> objects ); */
 // -------------------------------------------------------------------------
 
 
 // - Helper functions -------------------------------------------------------//
-int DisplayMenu( vector<string> options, bool zeroToReturn = true );
-int GetValidInput( int min, int max );
+int DisplayMenu(vector<string> options, bool zeroToReturn = true);
+int GetValidInput(int min, int max);
 void ClearScreen();
-void DisplayHeader( string title );
+void DisplayHeader(string title);
 void PressEnterToContinue();
 
 
