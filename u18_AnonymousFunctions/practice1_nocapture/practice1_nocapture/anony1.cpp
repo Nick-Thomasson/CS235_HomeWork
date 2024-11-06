f533b971dbd5357e27db71eae993b18af6d0cff1
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-11-05 18:12:14 -0600

    finish

--- u18_AnonymousFunctions/practice1_nocapture/practice1_nocapture/anony1.cpp
+++ u18_AnonymousFunctions/practice1_nocapture/practice1_nocapture/anony1.cpp
@@ -3,28 +3,27 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-  vector<string> data = { "cheese", "lettuce", "tomato", "mayo", "bread" };
+int main() {
+	vector<string> data = { "cheese", "lettuce", "tomato", "mayo", "bread" };
+	string findme;
+	cout << "Search for which term? ";
+	cin >> findme;
 
-  string findme;
-  cout << "Search for which term? ";
-  cin >> findme;
+	int match =
+		[data, findme]
+		(vector<string> input, string findme)
+		-> int
+		{
+			for (int i = 0; i < data.size(); i++) {
+				if (data[i] == findme) {
+					return i;
+				}
+			}
+			return -1;
+		}
+	(data, findme); // Function call with arguments
 
-  int match =
-    [] // Capture clause
-    ( vector<string> input, string findme ) // Parameters
-    -> int // Return type
-    {
-      // - STUDENT CODE ----------------------------------------------------------
-      // TODO: Implement linear search
+	cout << "Result: " << match << endl;
 
-      // -------------------------------------------------------------------------
-      return -1;
-    }
-    ( data, findme ); // Function call with arguments
-
-  cout << "Result: " << match << endl;
-
-  return 0;
+	return 0;
 }
