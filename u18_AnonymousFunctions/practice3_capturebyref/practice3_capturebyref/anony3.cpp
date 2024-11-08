7c6afebdaa64bbcedde552c32480873b7d2a858b
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-11-08 11:30:47 -0600

    finish

--- u18_AnonymousFunctions/practice3_capturebyref/practice3_capturebyref/anony3.cpp
+++ u18_AnonymousFunctions/practice3_capturebyref/practice3_capturebyref/anony3.cpp
@@ -3,44 +3,41 @@
 #include <string>
 using namespace std;
 
-void Display( const vector<string>& vec )
+void Display(const vector<string>& vec)
 {
-  cout << "[";
-  for ( size_t i = 0; i < vec.size(); i++ )
-    {
-      if ( i != 0 ) { cout << ", "; }
-      cout << i << "=" << vec[i];
-    }
-  cout << "]";
-  cout << endl;
+	cout << "[";
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		if (i != 0) { cout << ", "; }
+		cout << i << "=" << vec[i];
+	}
+	cout << "]";
+	cout << endl;
 }
 
 int main()
 {
-  vector<string> data = { "bread", "cheese", "lettuce", "tomato", "mayo", "bread" };
+	vector<string> data = { "bread", "cheese", "lettuce", "tomato", "mayo", "bread" };
+	cout << endl << "Vector before anonymous function:" << endl;
+	Display(data);
+	string findme, replacewith;
+	cout << endl << "Search for which term? ";
+	cin >> findme;
+	cout << "Replace with what? ";
+	cin >> replacewith;
+	[&]
+	()
+		-> void
+		{
 
-  cout << endl << "Vector before anonymous function:" << endl;
-  Display( data );
-
-  string findme, replacewith;
-  cout << endl << "Search for which term? ";
-  cin >> findme;
-  cout << "Replace with what? ";
-  cin >> replacewith;
-
-    [&] // Capture clause
-    () // Parameters
-    -> void // Return type
-    {
-      // - STUDENT CODE ----------------------------------------------------------
-      // TODO: Implement linear search
-
-      // -------------------------------------------------------------------------
-    }
-    (); // Function call with arguments
-
-    cout << endl <<  "Vector after anonymous function:" << endl;
-    Display( data );
-
-  return 0;
+			for (size_t i = 0; i < data.size(); i++) {
+				if (data[i] == findme) {
+					data[i] = replacewith;
+				}
+			}
+		}
+	();
+	cout << endl << "Vector after anonymous function:" << endl;
+	Display(data);
+	return 0;
 }
