33749aa950e63847c8ebfa528c9bcdbe7508647f
Nick Thomasson <nthomass@stumail.jccc.edu>
2024-11-17 10:03:33 -0600

    osh

--- u19_OverloadOperators/practice1_assignment/practice1_assignment/assignment.cpp
+++ u19_OverloadOperators/practice1_assignment/practice1_assignment/assignment.cpp
@@ -3,61 +3,58 @@
 #include <string>
 using namespace std;
 
-void Display( vector<float> grades )
+void Display(vector<float> grades)
 {
-  cout << "[";
-  for ( size_t i = 0; i < grades.size(); i++ )
-    {
-      if ( i != 0 ) { cout << ", "; }
-      cout << i << " = " << grades[i];
-    }
-  cout << "]" << endl;
+	cout << "[";
+	for (size_t i = 0; i < grades.size(); i++)
+	{
+		if (i != 0) { cout << ", "; }
+		cout << i << " = " << grades[i];
+	}
+	cout << "]" << endl;
 }
 
 class Grade
 {
 public:
-  void AddGrades( vector<float> grades )
-  {
-    this->grade_list = grades;
-  }
+	void AddGrades(vector<float> grades)
+	{
+		this->grade_list = grades;
+	}
 
-  vector<float> GetGrades()
-  {
-    return this->grade_list;
-  }
+	vector<float> GetGrades()
+	{
+		return this->grade_list;
+	}
 
-  // - STUDENT CODE ----------------------------------------------------------
-  Grade& operator=( const Grade& other )
-  {
-    if ( &other == this )
-      {
-        return *this;
-      }
-
-    // TODO: Implement
-    
-    return *this;
-  }
-  // -------------------------------------------------------------------------
+	// - STUDENT CODE ----------------------------------------------------------
+	Grade& operator=(const Grade& other)
+	{
+		if (&other == this)
+		{
+			//return *this;
+		}
+		this->grade_list = other.grade_list;
+		return *this;
+	}
+	// -------------------------------------------------------------------------
 
 private:
-  vector<float> grade_list;
+	vector<float> grade_list;
 };
 
 
 int main()
 {
-  Grade rai_grades, anuj_grades;
-  rai_grades.AddGrades( { 4.0, 3.5, 3.8 } );
-  // TODO: Uncomment out once you implement the = operator function
-  //anuj_grades = rai_grades;
+	Grade rai_grades, anuj_grades;
+	rai_grades.AddGrades({ 4.0, 3.5, 3.8 });
+	anuj_grades = rai_grades;
 
-  cout << endl << "rai_grades:" << endl;
-  Display( rai_grades.GetGrades() );
+	cout << endl << "rai_grades:" << endl;
+	Display(rai_grades.GetGrades());
 
-  cout << endl << "anuj_grades:" << endl;
-  Display( anuj_grades.GetGrades() );
+	cout << endl << "anuj_grades:" << endl;
+	Display(anuj_grades.GetGrades());
 
-  return 0;
+	return 0;
 }
